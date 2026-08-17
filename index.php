9bf4e91888b4e526f5e30d8b1e09e715689b2a7a
natanvitor550 <natan_v_barbosa@estudante.sesisenai.org.br>
2026-08-17 10:51:45 -0300

    add - Index

--- index.php
+++ index.php
@@ -7,10 +7,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <title>Cadastro</title>
 </head>
 <body>
-    
+    <h1> Cadastro de Usuário </h1>
+    <label>Usuário:</label> 
+
+
 </body>
 </html>
