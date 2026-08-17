15f0f55dcdf97cdd09a7be41eec0a014a44fedca
natanvitor550 <natan_v_barbosa@estudante.sesisenai.org.br>
2026-08-17 11:22:40 -0300

    up - index

--- index.php
+++ index.php
@@ -10,13 +10,21 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-    <title>Cadastro</title>
+    <title>Cadastro de Usuário</title>
 </head>
 <body>
     <h1> Cadastro de Usuário </h1>
-    <label for="email">E-mail:</label>
+    <label for="email">E-mail: </label>
     <input type="email" id="email" name="email"> 
+    
+    
+    <div class=flex>
+    <label for="nome"> Nome: </label>
+    <input type="text" id="nome" name="nome">
+</div>
 
-
+<div class=flex>
+    <input type="submit" id="enviar" name="enviar">
+</div>
 </body>
 </html>
